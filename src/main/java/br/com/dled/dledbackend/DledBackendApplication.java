e9ec11ba6f4f2232bfff88b65419168dbd8a3b39
jvictornascimento <victojoao789@gmail.com>
2026-04-07 11:00:23 -0300

    chore: start project

---
+++ src/main/java/br/com/dled/dledbackend/DledBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.dled.dledbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DledBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DledBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dled/dledbackend/DledBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dled.dledbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DledBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
